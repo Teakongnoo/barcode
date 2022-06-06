bd4ffd8777e3e3053bf796e63aa22af123c7cd24
Teakongnoo <40262506+Teakongnoo@users.noreply.github.com>
2022-06-06 20:42:16 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-			host: env('DATABASE_HOST', '127.0.0.1'),
+			host: env('DATABASE_HOST', 'ec2-52-4-104-184.compute-1.amazonaws.com'),
 			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'barcode'),
-			user: env('DATABASE_USERNAME', 'postgres'),
-			password: env('DATABASE_PASSWORD', '1234'),
+			database: env('DATABASE_NAME', 'd7tri15n0v2lta'),
+			user: env('DATABASE_USERNAME', 'xtqlahmshrgyif'),
+			password: env('DATABASE_PASSWORD', 'e1eb0572443e9fe42c112977d43153c9c0bb7de5d99350f16e468d88c7cf33c2'),
 			ssl: env.bool('DATABASE_SSL', true),
 			//ssl: false
 		}
